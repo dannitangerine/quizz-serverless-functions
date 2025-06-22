b9476007ceec9e794e7de26762901dc900438d60
Daniel Vargas Tapia <daniel@Mac-mini-de-Daniel.local>
2025-06-21 18:27:00 -0600

    cors added

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
 console.log(req.method)
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.status(200).json({ mensaje: 'Hola desde vercel' });
 }
